63dcde802b54a3ae4fd25dea968a8f85fe97c13a
Jane Shim <jihyunshim08@gmail.com>
2021-03-24 16:47:01 -0700

    created a first array of object in javascript

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,22 @@
-// your JavaScript goes here, you can delete this comment
+
+let todoList = [
+{
+    todo: "clone repo for starter code",
+    status: "started"
+},
+{
+    todo: "clone repo for starter code",
+    status: "completed"
+},
+{
+    todo: "interate over a loop",
+    status: "started"
+
+},
+{
+    todo: "iterate over a loop",
+    status: "completed"
+}
+]
+
+
